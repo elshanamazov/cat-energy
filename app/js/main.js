4291cc0bea934600a821cbdae7795664c3a5c3ba
Honor <mytwink21@mail.ru>
2022-06-08 15:23:38 +0100

    header section

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,11 @@
-import {testFunction} from './scripts/test';
-testFunction();
+// burger 
+
+const burger = document.querySelector('.js-burger'),
+		menu = document.querySelector('.js-nav');
+
+burger.addEventListener('click', function(){
+	burger.classList.toggle('_active');
+	menu.classList.toggle('_active');
+})
+
+
